c630fb46210918ad4e4e39a03ae5b35a93026011
Marcos Martins de Queiroz <marcos@beecoffee.com.br>
2020-12-27 18:53:01 -0300

    feat(heroku): publicação

--- www/html/sonda/app/Controllers/Api.php
+++ www/html/sonda/app/Controllers/Api.php
@@ -2,42 +2,42 @@
 
 use CodeIgniter\API\ResponseTrait;
 
-class Api extends Home
+class Api extends Sonda
 {
-    use ResponseTrait;
-    
-    public function init()
-    {
-      $this->start();
+  use ResponseTrait;
+  
+  public function init()
+  {
+    $this->start();
 
-      if(isset($this->session->face)){
-        return $this->displaysCoordinates();
-      } else {
-        return $this->respond(['error' => 'Não foi possível iniciar o terreno para a sonda.']);
-      }
+    if(isset($this->session->face)){
+      return $this->displaysCoordinates();
+    } else {
+      return $this->respond(['error' => 'Não foi possível iniciar o terreno para a sonda.']);
     }
+  }
 
-    public function receivesCommand()
-    {
-      $command = $this->request->getJSON()->movimentos;
+  public function receivesCommand()
+  {
+    $command = $this->request->getJSON()->movimentos;
 
-      $respond = $this->command($command);
+    $respond = $this->command($command);
 
-      if($respond['status']){
-        return $this->respond($respond['position']);
-      } else {
-        return $this->respond(['error' => $respond['erro']]);
-      }
+    if($respond['status']){
+      return $this->respond($respond['position']);
+    } else {
+      return $this->respond(['error' => $respond['erro']]);
     }
+  }
 
-    public function displaysCoordinates()
-    {
-      $respond = array(
-        'x' => $this->session->coodX,
-        'y' => $this->session->coodY,
-        'face' => $this->session->face
-      );
+  public function displaysCoordinates()
+  {
+    $respond = array(
+      'x' => $this->session->coodX,
+      'y' => $this->session->coodY,
+      'face' => $this->session->face
+    );
 
-      return $this->respond($respond);
-    }
+    return $this->respond($respond);
+  }
 }
