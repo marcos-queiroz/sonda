2ec399a69ea976c077e752477db7b492776509fb
Marcos Martins de Queiroz <marcos@beecoffee.com.br>
2020-12-28 11:33:05 -0300

    feat(vender): upload
    upload do vendor para rodar em maquinas sem o composer

--- www/html/sonda/tests/api/ApiTest.php
+++ www/html/sonda/tests/api/ApiTest.php
@@ -4,7 +4,6 @@ use CodeIgniter\Test\CIUnitTestCase;
 
 class ApiTest extends CIUnitTestCase
 {   
-
     public function setUp(): void
     {
         parent::setUp();
@@ -17,4 +16,24 @@ class ApiTest extends CIUnitTestCase
 
         $this->assertEquals('201', $response->getStatusCode());
     }
+
+    // public function testReceivesCommand()
+    // {
+    //     $client = \Config\Services::curlrequest();
+    //     $response = $client->request('POST', base_url('api/receivesCommand'), ['json' => ['movimentos' => ["GE", "M", "M", "M", "GD", "M", "M"]]]);
+
+    //     // $body = $response->getBody();
+
+    //     $this->assertEquals('201', $response->getStatusCode());
+    // }
+
+    // public function testDisplaysCoordinates()
+    // {
+    //     $client = \Config\Services::curlrequest();
+    //     $response = $client->request('GET', base_url('api/init'));
+
+    //     // $body = $response->getBody();
+
+    //     $this->assertEquals('201', $response->getStatusCode());
+    // }
 }
